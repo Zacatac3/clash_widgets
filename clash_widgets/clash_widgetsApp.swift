abbf8759d2a1b64eeb5e474bc6414dba492feeda
Zachary T. Buschmann <zbuschm@rockets.utoledo.edu>
2026-01-07 20:09:36 -0500

    Initial Commit

---
+++ clash_widgets/clash_widgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  clash_widgetsApp.swift
+//  clash_widgets
+//
+//  Created by Zachary Buschmann on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct clash_widgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
